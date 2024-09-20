52f41a3944219f5d15597471e13f53b177dac1a6
sm-mehedi <hasanmehedi63395@gmail.com>
2024-09-20 13:28:53 +0600

    home page added

--- Script/login.js
+++ Script/login.js
@@ -5,5 +5,10 @@ document.getElementById('btn-login').addEventListener('click',function(event){
     console.log('Login button clicked');
     const phone = document.getElementById('phone-number').value;
     console.log(phone);
+    const pin_value = document.getElementById('pin-field').value;
+    console.log(pin_value);
+    document.getElementById('phone-number').value="";
+    document.getElementById('pin-field').value="";
+    window.location.href='./home.html';
 
 })
